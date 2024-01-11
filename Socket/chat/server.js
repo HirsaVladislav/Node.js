af81cbc9cdc55242caa1ffd0a54b2877d5c61835
HirsaVladislav <hirsavlad@gmail.com>
2024-01-11 23:35:31 +0200

    chat

---
+++ Socket/chat/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+const PORT = 3000;
+
+// Send html file by root path: http://localhost:3000/
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './front.html'))
+});
+
+// Run our server
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
+
+io.on('connection', (socket) => {
+  console.log(`User connected to socket by id: ${socket.id}`);
+
+  // Listen event from client
+  socket.on('message', (msg) => {
+    console.log('Income message', msg);
+
+    // Send message to client
+    socket.emit('server_answer', `Server has received your message: ${msg}`)
+  })
+});
+
